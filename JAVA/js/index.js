7b1dec484ad69da07d47bb67b10bc3876577ce04
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2022-11-09 21:28:11 +0900

    11.09 lec HW

--- JAVA/js/index.js
+++ JAVA/js/index.js
@@ -1,7 +1,3 @@
-function showAlertBox() {
-    document.querySelector(".alert-box").style.display = "block"
-}
-
-function closeAlertBox() {
-    document.querySelector(".alert-box").style.display = "none"
+function changeParagraph(contents) {
+    document.querySelector(".para").innerHTML = contents;
 }
